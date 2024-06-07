1e79cab77253a5aa35eb61ecb0e2c1b9055e6282
matthewloh <64674205+matthewloh@users.noreply.github.com>
2024-06-07 20:21:28 +0800

    add rewrite to fastapi example for prisma in backend

--- api/index.py
+++ api/index.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
-
+from api.src.prisma import prisma
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    await prisma.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await prisma.disconnect()
+
 
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
 
+
 @app.get("/api/python/{name}")
 async def hello_name(name: str):
-    return {"message": f"Hello {name}"}
+    mvac = await prisma.malaysiavaccination.find_first()
+    return {"message": f"{mvac.model_dump()}"}
