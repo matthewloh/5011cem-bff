4af670ea492c8cb9df3ecff0f25f7a2b4b99736e
wzthor <wzthor2002@gmail.com>
2024-07-07 17:16:21 +0800

    Add state vax by brand query files

---
+++ app/(dashboard)/dashboard/(dataset)/state-vaccinations/getFirstDoseByBrand.ts
@@ -0,0 +1,51 @@
+import prisma from "@/lib/db";
+import { getChartDateArray } from "@/utils/formatters";
+import { Prisma } from "@prisma/client";
+import { startOfDay } from "date-fns";
+
+export async function getFirstDoseByBrand(
+  recordedAfter: Date | null,
+  recordedBefore: Date | null,
+  selectedState: string | undefined,
+) {
+  const recordedAtQuery: Prisma.StateVaccinationWhereInput["date"] = {};
+  if (recordedAfter) recordedAtQuery.gte = recordedAfter;
+  if (recordedBefore) recordedAtQuery.lte = recordedBefore;
+
+  const [
+    // Data
+    chartData,
+  ] = await Promise.all([
+    prisma.stateVaccination.findMany({
+      select: {
+        id: true,
+        date: true,
+        pfizer1: true,
+        sinovac1: true,
+        astra1: true,
+        sinopharm1: true,
+        cansino: true,
+        pending1: true,
+      },
+      where: {
+        date: recordedAtQuery,
+        state: selectedState,
+      },
+      orderBy: {
+        date: "asc",
+      },
+    }),
+  ]);
+
+  const { array, format } = getChartDateArray(
+    recordedAfter || startOfDay(chartData[0].date!),
+    recordedBefore || new Date(),
+  );
+  
+  return {
+    chartData: chartData.map((day) => ({
+      ...day,
+      date: format(day.date!),
+    }))
+  };
+}
---
+++ app/(dashboard)/dashboard/(dataset)/state-vaccinations/getThirdDoseByBrand.ts
@@ -0,0 +1,51 @@
+import prisma from "@/lib/db";
+import { getChartDateArray } from "@/utils/formatters";
+import { Prisma } from "@prisma/client";
+import { startOfDay } from "date-fns";
+
+export async function getThirdDoseByBrand(
+  recordedAfter: Date | null,
+  recordedBefore: Date | null,
+  selectedState: string | undefined,
+) {
+  const recordedAtQuery: Prisma.StateVaccinationWhereInput["date"] = {};
+  if (recordedAfter) recordedAtQuery.gte = recordedAfter;
+  if (recordedBefore) recordedAtQuery.lte = recordedBefore;
+
+  const [
+    // Data
+    chartData,
+  ] = await Promise.all([
+    prisma.stateVaccination.findMany({
+      select: {
+        id: true,
+        date: true,
+        pfizer3: true,
+        sinovac3: true,
+        astra3: true,
+        sinopharm3: true,
+        cansino3: true,
+        pending3: true,
+      },
+      where: {
+        date: recordedAtQuery,
+        state: selectedState,
+      },
+      orderBy: {
+        date: "asc",
+      },
+    }),
+  ]);
+
+  const { array, format } = getChartDateArray(
+    recordedAfter || startOfDay(chartData[0].date!),
+    recordedBefore || new Date(),
+  );
+  
+  return {
+    chartData: chartData.map((day) => ({
+      ...day,
+      date: format(day.date!),
+    }))
+  };
+}
