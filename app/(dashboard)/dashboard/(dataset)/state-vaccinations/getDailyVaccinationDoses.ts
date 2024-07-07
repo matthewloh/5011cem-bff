43302c18ed98b09a46d13b45dcefb9696777f06b
wzthor <wzthor2002@gmail.com>
2024-07-07 17:15:22 +0800

    Add state vax overview query files

---
+++ app/(dashboard)/dashboard/(dataset)/state-vaccinations/getDailyVaccinationDoses.ts
@@ -0,0 +1,49 @@
+import prisma from "@/lib/db";
+import { getChartDateArray } from "@/utils/formatters";
+import { Prisma } from "@prisma/client";
+import { startOfDay } from "date-fns";
+
+export async function getDailyVaccinationDoses(
+  recordedAfter: Date | null,
+  recordedBefore: Date | null,
+  selectedState: string | undefined,
+) {
+  const recordedAtQuery: Prisma.StateVaccinationWhereInput["date"] = {};
+  if (recordedAfter) recordedAtQuery.gte = recordedAfter;
+  if (recordedBefore) recordedAtQuery.lte = recordedBefore;
+
+  const [
+    // Data
+    chartData,
+  ] = await Promise.all([
+    prisma.stateVaccination.findMany({
+      select: {
+        id: true,
+        date: true,
+        daily_partial: true,
+        daily_full: true,
+        daily_booster: true,
+        daily_booster2: true,
+      },
+      where: {
+        date: recordedAtQuery,
+        state: selectedState,
+      },
+      orderBy: {
+        date: "asc",
+      },
+    }),
+  ]);
+
+  const { array, format } = getChartDateArray(
+    recordedAfter || startOfDay(chartData[0].date!),
+    recordedBefore || new Date(),
+  );
+
+  return {
+    chartData: chartData.map((day) => ({
+      ...day,
+      date: format(day.date!),
+    }))
+  };
+}
